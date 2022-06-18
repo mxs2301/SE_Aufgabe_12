77db4a33dff69fffbafaebb208c96593c3eaed40
Max Schopf <max.schopf@mailfence.com>
2022-06-18 20:17:09 +0200

    Update um zu Testen ob es klappen würde

--- src/main/java/com/schopf/Main.java
+++ src/main/java/com/schopf/Main.java
@@ -1,6 +1,16 @@
 package com.schopf;
-public class Main{
-	public static void main(String[] args){
-		System.out.println("H");
+
+public class Main {
+	public static void main(String[] args) {
+
+		SpecificSubject alphaSubject = new SpecificSubject();
+
+		System.out.println(alphaSubject.getState());
+		SpecificSubscriber newyork = new SpecificSubscriber(alphaSubject);
+
+		alphaSubject.attach(newyork);
+
+		alphaSubject.setState(1);
+
 	}
 }
