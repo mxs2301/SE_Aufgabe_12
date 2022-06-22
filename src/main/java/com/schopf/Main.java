7b0714885130f9ed89d7e931afb4d60ccf9ed885
Max Schopf <max.schopf@mailfence.com>
2022-06-22 13:59:21 +0200

    Update

--- src/main/java/com/schopf/Main.java
+++ src/main/java/com/schopf/Main.java
@@ -2,7 +2,13 @@ package com.schopf;
 
 public class Main {
 	public static void main(String[] args) {
+		SpecificSubject subject = new SpecificSubject();
+		SpecificSubscriber subscriber = new SpecificSubscriber(subject);
 
+		System.out.println(subject.getState());
+		subject.attach(subscriber);
+
+		subject.setState(1);
 		
 	}
 }
